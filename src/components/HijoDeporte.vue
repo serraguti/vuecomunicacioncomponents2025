2f63abdc3fc967c933c6571c377581dc8eda21fc
serraguti <pacoserranox@gmail.com>
2025-10-27 14:15:40 +0100

    first commit

---
+++ src/components/HijoDeporte.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h4 style="color:red">{{ nombredeporte }}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HijoDeporte",
+        props: ["nombredeporte"]
+        , mounted() {
+            console.log(this.nombredeporte)
+        }
+    }
+</script>
---
+++ src/components/PadreDeportes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 style="color:blue">Padre deportes</h1>
+        <ul>
+            <li v-for="sport in deportes" :key="sport">
+                <HijoDeporte :nombredeporte="sport"/>
+            </li>
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+import HijoDeporte from './HijoDeporte.vue';
+    export default {
+        name: "PadreDeportes",
+        components: {
+            HijoDeporte
+        },
+        data() {
+            return {
+                deportes: ["Curling", "Peonza", "Canicas", "Bolos"]
+            }
+        }
+    }
+</script>
